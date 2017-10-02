5a57ad0d5e798b089f33229fe86432e466da3800
Hongxia Shi <Hongxia@pal-nat186-50-133.itap.purdue.edu>
2017-10-02 11:54:37 -0400

    updated

--- server.R
+++ server.R
@@ -1,35 +1,286 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+### load the original data
+myurl <- "https://raw.githubusercontent.com/Hongxia555/UR4A_project/master/german.data.csv"
+credit <- read.table(file=myurl, header=T, sep = ',')
+# change the column names
+names(credit) <- c("AccountBalance", "DurationInMonth", "PaymentStatus"
+                   , "PurposeOfCredit", "CreditAmount", "SavingOrBonds"
+                   , "LengthOfCurrentEmployment", "Installment", "SexAndMaritalStatus"
+                   , "Guarantor", "DurationInCurrentAddress", "MostValuableAsset"
+                   , "Age", "OtherInstallmentPlan", "Housing", "NoOfCreditsAtBank"
+                   , "Occupation", "NoOfDependents", "Telephone", "ForeignWorker"
+                   , "CreditEvaluation")
 
+### import packages
 library(shiny)
+library(rgl)
+library(useful)
+library(ggplot2)
+options(rgl.printRglwidget = TRUE)
+
+### convert factor variables into factor variables
+# convert variable AccountBalance
+credit$accountBalance[credit$AccountBalance=="A11"] <- 1
+credit$accountBalance[credit$AccountBalance=="A12"] <- 2
+credit$accountBalance[credit$AccountBalance=="A13"] <- 3
+credit$accountBalance[credit$AccountBalance=="A14"] <- 4
+credit$accountBalance <- as.factor(credit$accountBalance)
+credit$AccountBalance <- credit$accountBalance
+credit$accountBalance <- NULL
+# convert variable PaymentStatus
+credit$paymentStatus[credit$PaymentStatus=="A30"] <- 1
+credit$paymentStatus[credit$PaymentStatus=="A31"] <- 2
+credit$paymentStatus[credit$PaymentStatus=="A32"] <- 3
+credit$paymentStatus[credit$PaymentStatus=="A33"] <- 4
+credit$paymentStatus[credit$PaymentStatus=="A34"] <- 5
+credit$paymentStatus <- as.factor(credit$paymentStatus)
+credit$PaymentStatus <- credit$paymentStatus
+credit$paymentStatus <- NULL
+# convert variable PurposeOfCredit
+credit$purposeOfCredit[credit$PurposeOfCredit=='A40'] <- 1
+credit$purposeOfCredit[credit$PurposeOfCredit=='A41'] <- 2
+credit$purposeOfCredit[credit$PurposeOfCredit=='A42'] <- 3
+credit$purposeOfCredit[credit$PurposeOfCredit=='A43'] <- 4
+credit$purposeOfCredit[credit$PurposeOfCredit=='A44'] <- 5
+credit$purposeOfCredit[credit$PurposeOfCredit=='A45'] <- 6
+credit$purposeOfCredit[credit$PurposeOfCredit=='A46'] <- 7
+credit$purposeOfCredit[credit$PurposeOfCredit=='A47'] <- 8
+credit$purposeOfCredit[credit$PurposeOfCredit=='A48'] <- 9
+credit$purposeOfCredit[credit$PurposeOfCredit=='A49'] <- 10
+credit$purposeOfCredit[credit$PurposeOfCredit=='A410'] <- 11
+credit$purposeOfCredit <- as.factor(credit$purposeOfCredit)
+credit$PurposeOfCredit <- credit$purposeOfCredit
+credit$purposeOfCredit <- NULL
+# convert the variable SavingOrBonds
+credit$savingOrBonds[credit$SavingOrBonds=='A61'] <- 1
+credit$savingOrBonds[credit$SavingOrBonds=='A62'] <- 2
+credit$savingOrBonds[credit$SavingOrBonds=='A63'] <- 3
+credit$savingOrBonds[credit$SavingOrBonds=='A64'] <- 4
+credit$savingOrBonds[credit$SavingOrBonds=='A65'] <- 5
+credit$savingOrBonds <- as.factor(credit$savingOrBonds)
+credit$SavingOrBonds <- credit$savingOrBonds
+credit$savingOrBonds <- NULL
+# convert the variable LengthOfCurrentEmployment
+credit$lengthOfCurrentEmployment[credit$LengthOfCurrentEmployment=="A71"] <- 1
+credit$lengthOfCurrentEmployment[credit$LengthOfCurrentEmployment=="A72"] <- 2
+credit$lengthOfCurrentEmployment[credit$LengthOfCurrentEmployment=="A73"] <- 3
+credit$lengthOfCurrentEmployment[credit$LengthOfCurrentEmployment=="A74"] <- 4
+credit$lengthOfCurrentEmployment[credit$LengthOfCurrentEmployment=="A75"] <- 5
+credit$lengthOfCurrentEmployment <- as.numeric(credit$lengthOfCurrentEmployment)
+credit$LengthOfCurrentEmployment <- credit$lengthOfCurrentEmployment
+credit$lengthOfCurrentEmployment <- NULL
+# convert the variable SexAndMaritalStatus
+credit$sexAndMaritalStatus[credit$SexAndMaritalStatus=="A91"] <- 1
+credit$sexAndMaritalStatus[credit$SexAndMaritalStatus=="A92"] <- 2
+credit$sexAndMaritalStatus[credit$SexAndMaritalStatus=="A93"] <- 3
+credit$sexAndMaritalStatus[credit$SexAndMaritalStatus=="A94"] <- 4
+credit$sexAndMaritalStatus[credit$SexAndMaritalStatus=="A95"] <- 5
+credit$sexAndMaritalStatus <- as.factor(credit$sexAndMaritalStatus)
+credit$SexAndMaritalStatus <- credit$sexAndMaritalStatus
+credit$sexAndMaritalStatus <- NULL
+# convert the variable Guarantor
+credit$guarantor[credit$Guarantor=="A101"] <- 1
+credit$guarantor[credit$Guarantor=="A102"] <- 2
+credit$guarantor[credit$Guarantor=="A103"] <- 3
+credit$guarantor <- as.factor(credit$guarantor)
+credit$Guarantor <- credit$guarantor
+credit$guarantor <- NULL
+# convert the variable MostValuableAsset
+credit$mostValuableAsset[credit$MostValuableAsset=='A121'] <- 1
+credit$mostValuableAsset[credit$MostValuableAsset=='A122'] <- 2
+credit$mostValuableAsset[credit$MostValuableAsset=='A123'] <- 3
+credit$mostValuableAsset[credit$MostValuableAsset=='A124'] <- 4
+credit$mostValuableAsset <- as.factor(credit$mostValuableAsset)
+credit$MostValuableAsset <- credit$mostValuableAsset
+credit$mostValuableAsset <- NULL
+# convert the variable OtherInstallmentPlan
+credit$otherInstallmentPlan[credit$OtherInstallmentPlan=='A141'] <- 1
+credit$otherInstallmentPlan[credit$OtherInstallmentPlan=='A142'] <- 2
+credit$otherInstallmentPlan[credit$OtherInstallmentPlan=='A143'] <- 3
+credit$otherInstallmentPlan <- as.factor(credit$otherInstallmentPlan)
+credit$OtherInstallmentPlan <- credit$otherInstallmentPlan
+credit$otherInstallmentPlan <- NULL
+# convert the variable Housing
+credit$housing[credit$Housing=='A151'] <- 1
+credit$housing[credit$Housing=='A152'] <- 2
+credit$housing[credit$Housing=='A153'] <- 3
+credit$housing <- as.factor(credit$housing)
+credit$Housing <- credit$housing
+credit$housing <- NULL
+# convert the variable Occupation
+credit$occupation[credit$Occupation=='A171'] <- 1
+credit$occupation[credit$Occupation=='A172'] <- 2
+credit$occupation[credit$Occupation=='A173'] <- 3
+credit$occupation[credit$Occupation=='A174'] <- 4
+credit$occupation <- as.factor(credit$occupation)
+credit$Occupation <- credit$occupation
+credit$occupation <- NULL
+# convert the variable Telephone
+credit$telephone[credit$Telephone=='A191'] <- 1
+credit$telephone[credit$Telephone=='A192'] <- 2
+credit$telephone <- as.factor(credit$telephone)
+credit$Telephone <- credit$telephone
+credit$telephone <- NULL
+# convert the variable ForeignWorker
+credit$foreignWorker[credit$ForeignWorker=='A201'] <- 1
+credit$foreignWorker[credit$ForeignWorker=='A202'] <- 2
+credit$foreignWorker <- as.factor(credit$foreignWorker)
+credit$ForeignWorker <- credit$foreignWorker
+credit$foreignWorker <- NULL
+
+###convert CreditEvaluation into Good:1 and Bad:0
+credit$CreditEvaluation[credit$CreditEvaluation==2] <-0 
+credit$CreditEvaluation <- as.factor(credit$CreditEvaluation)
+
+###set train data and test data
+smp_size <- floor(0.75 * nrow(credit))
+# set the seed to make the partition reproductible
+set.seed(107)
+train_credit <- sample(seq_len(nrow(credit)), size = smp_size)
+train <- credit[train_credit, ]
+test <- credit[-train_credit, ]
+run <- sample(seq_len(nrow(credit)), size = 1)
+run_credit <- credit[run, ]
+
+#Logit
+logit <- glm(CreditEvaluation ~ . 
+             , data=train, family=binomial)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-   # output$logo <- renderImage(
-     # "https://drive.google.com/file/d/0B3epwLPCymTtZER6cXpCS25MdGs/view?usp=sharing"
-     
-   # )
-  
-  
-  output$distPlot <- renderPlot({
+
+  ##ClusterPlot
+  output$ClusterPlot <- renderPlot({
+    creditTrain <- credit[, which(names(credit) != "CreditEvaluation")]
+    if  (input$AccountBalance1==FALSE)
+      creditTrain <- credit[, which(names(credit) != "AccountBalance")]
+    if  (input$DurationInMonth1==FALSE)
+      creditTrain <- credit[, which(names(credit) != "DurationInMonth")]
+    if  (input$PurposeOfCredit1==FALSE)
+      creditTrain <- credit[, which(names(credit) != "PurposeOfCredit")]
+    if  (input$DurationInCurrentAddress1==FALSE)
+      creditTrain <- credit[, which(names(credit) != "DurationInCurrentAddress")]
+    if  (input$Age1==FALSE)
+      creditTrain <- credit[, which(names(credit) != "Age")]
+    if  (input$NoOfCreditsAtBank1==FALSE)
+      creditTrain <- credit[, which(names(credit) != "NoOfCreditsAtBank")]
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+    # creditTrain <- credit[, which(names(credit) != "CreditEvaluation")]
+    # set the numbers of clustering
+    creditBest <- FitKMeans(creditTrain, max.clusters=20, nstart=10, seed=278613)
+    k <- creditBest[creditBest$AddCluster != "TRUE", ][1,1]
+    # perform k-means clustering
+    # two dimensional clustering
+    creditK <- kmeans(creditTrain, k)
+    plot(creditK, data=credit, class="CreditEvaluation")
   })
+    
+   
   
-  # output$team <- renderText({
-  #   paste("hello", "world", sep="\n"))
-  # 
+  ##Prediction Plot
+   output$PredictionPlot <- renderPlot({
+     
+     costOfgood <- input$BadCustomer
+     costOfbad <- input$GoodCustomer
+     
+     #Optimization
+     x <- 0 
+     finalresults <- NULL
+     while(x < 0.99) {
+       x <- x + 0.01
+       pLogit <- predict(logit, type="response",train)      # predicted probabilities
+       yLogit <- ifelse(pLogit>=x,"Good","Bad")      # actual class labels
+       results = data.frame(train$CreditEvaluation, yLogit, pLogit)
+       colnames(results) <- c("y","yLogit","pLogit")
+       # head(results)
+       
+       cm <- table(results$yLogit, results$y)
+       if(nrow(cm)==2){cost <- cm[[1,2]]*costOfbad + cm[[2,1]]*costOfgood}
+       else{cost <-cm[[1,1]]*costOfbad}
+       accuracy <- sum(diag(cm))/sum(cm)
+       finalresults = rbind(finalresults, data.frame(x,cost, accuracy))
+       # print(finalresults)
+       if (x == 1) break
+     }
+     
+     # print(finalresults)
+     
+     #entrydata
+     library(ggplot2)
+     ggplot(finalresults, aes(x = finalresults$x, y = finalresults$cost)) + geom_line() + geom_point()
+     
+   })
+  
+  
+  ##PredictionTable
+   output$PredictionTable <- renderTable({
+      
+      
+      
+      # save input to a dataframe
+      
+      AccountBalance <- as.factor(input$AccountBalance)
+      DurationInMonth <- as.integer(input$DurationInMonth)
+      PaymentStatus <- as.factor(input$PaymentStatus)
+      PurposeOfCredit <- as.factor(input$PurposeOfCredit) 
+      CreditAmount <- as.integer(input$CreditAmount)
+      SavingOrBonds <- as.factor(input$SavingOrBonds)
+      LengthOfCurrentEmployment <- as.numeric(input$LengthOfCurrentEmployment) 
+      Installment <- as.integer(input$Installment)
+      SexAndMaritalStatus <- as.factor(input$SexAndMaritalStatus)
+      Guarantor <- as.factor(input$Guarantor)
+      DurationInCurrentAddress <- as.integer(input$DurationInCurrentAddress)
+      MostValuableAsset <- as.factor(input$MostValuableAsset)
+      Age <- as.integer(input$Age)
+      OtherInstallmentPlan <- as.factor(input$OtherInstallmentPlan)
+      Housing <- as.factor(input$Housing) 
+      NoOfCreditsAtBank <- as.integer(input$NoOfCreditsAtBank)
+      Occupation <- as.factor(input$Occupation)
+      NoOfDependents <- as.integer(input$NoOfDependents)
+      Telephone <- as.factor(input$Telephone)
+      ForeignWorker <- as.factor(input$ForeignWorker)
+      
+      dataEntry <- data.frame(AccountBalance,
+                              DurationInMonth,
+                              PaymentStatus,
+                              PurposeOfCredit,  
+                              CreditAmount, 
+                              SavingOrBonds, 
+                              LengthOfCurrentEmployment,  
+                              Installment, 
+                              SexAndMaritalStatus, 
+                              Guarantor, 
+                              DurationInCurrentAddress,
+                              MostValuableAsset,
+                              Age,
+                              OtherInstallmentPlan,
+                              Housing,
+                              NoOfCreditsAtBank,
+                              Occupation,
+                              NoOfDependents,
+                              Telephone,
+                              ForeignWorker)
+      
+      names(dataEntry) <- c("AccountBalance", "DurationInMonth", "PaymentStatus"
+                         , "PurposeOfCredit", "CreditAmount", "SavingOrBonds"
+                         , "LengthOfCurrentEmployment", "Installment", "SexAndMaritalStatus"
+                         , "Guarantor", "DurationInCurrentAddress", "MostValuableAsset"
+                         , "Age", "OtherInstallmentPlan", "Housing", "NoOfCreditsAtBank"
+                         , "Occupation", "NoOfDependents", "Telephone", "ForeignWorker"
+                         )
+      
+  
+      #Prodiction
+      
+      i <- input$i
+      
+      pLogit <- predict(logit, type="response", dataEntry)      # predicted probabilities
+      yLogit <- ifelse(pLogit>=i,"Good","Bad")      # actual class labels
+      results = data.frame(yLogit, pLogit)
+      colnames(results) <- c("Recommended Classification","Predicted Probability of Good")
+      head(results)
+      
+      
+      
+    })
+    
 })
--- ui.R
+++ ui.R
@@ -1,99 +1,168 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(navbarPage(title=strong("Predict The Risk Level Of Customers"),
-  
+shinyUI( navbarPage(title=strong("SMART LOAN"),
         # Tab for introduction
-        tabPanel("INTRODUCTION",
-                 h1(strong("The Risk Level of Customers")),
+        tabPanel("Introduction",
+                 h1(strong("Predict credit risk level of customers")),
                  hr(),
-                 p(strong("Introduction")),
-                 p("This app is used to ...."),
+                 p(strong("Overview")),
+                 p("This app is designed to help money lending companies to decide whether to give a loan to a customer or not."),
                  br(),
-                 p(strong("Team:"), "Bubble Tea Rocks"),
-                 p(strong("Team members:"), "Jou Tzu (Rose) Kao | Liao (Alice) Rong | Hongxia Shi | Shenyang Yang"),
+                 p(strong("Designed by:"), "Bubble Tea Rocks"),
+                 p(strong("Team members:"), "Jou Tzu (Rose) Kao | Rong (Alice) Liao | Hongxia Shi | Shenyang Yang"),
+                 p(strong("Contact Information:"),"shi395@purdue.edu"),
                  
                  # imageOutput("logo")
-                 img(src="filelist.xml", 
-                     href = "./logo.fld")
+                 img(src="bubble.png")
+                     #href = "~/Dropbox/Study/Purdue/BAIM/Fall/MGMT590 Using R for Analytics/Project/")
                      # href = "https://drive.google.com/file/d/0B3epwLPCymTtZER6cXpCS25MdGs/view?usp=sharing")
-                 
-                 # img(src = "us-cdc.png", alt = "US CDC"),
-                 # href = "http://wonder.cdc.gov/cancer.html",
-                 # target = "_blank"
-           
-           
-                 ),
-                         
-        # Tab for prediction
-        tabPanel("Prediction",
-                 h1(strong("Pridict The Risk Level of Customers")),
-                 hr(),
-                 titlePanel("Predict The Risk Level Of Customers"),
-                 "contents"
                  ),
         
-        # Tab for clustering
-        tabPanel("Clustering",
-                  h1(strong("Cluster the data")),
-                  hr(),
-                  titlePanel("Predict The Risk Level Of Customers"),
-                  
-                 # Sidebar with a slider input for number of bins 
-                  sidebarLayout(
-                    sidebarPanel(
-                      # Choose first column for x axis of 3D visulization
-                      selectInput(
-                        "column1", "Choose x axis",
-                        c("Risk level" = "all"),
-                        selected = "Risk level"), 
-                      
-                      # Choose second column for y axis of 3D visulization
-                      selectInput(
-                        "column2", "Choose y axis",
-                        c("Status of existing checking account" = "Status", "Duration in month" = "Duration"),
-                        selected = "Status"), 
-                      
-                      
-                      # Choose third column for z axis of 3D visulization
-                      selectInput(
-                        "column3", "Choose z axis",
-                        c("Status of existing checking account" = "Status", "Duration in month" = "Duration"),
-                        selected = "Duration"), 
-                      
-                      
-                      
-                      
-                      sliderInput("bins",
-                                   "Number of bins:",
-                                   min = 1,
-                                   max = 50,
-                                   value = 30),
-                      
-                      # Generate the logo image
-                      br(), br(), br(), br(),
-                      
-                              a("the United States CDC",
-                          href = "http://wonder.cdc.gov/cancer.html",
-                          target = "_blank"),
-                      a(img(src = "us-cdc.png", alt = "US CDC"),
-                        href = "http://wonder.cdc.gov/cancer.html",
-                        target = "_blank")
-            ),
-            
-            # Show a plot of the generated distribution
-            mainPanel(
-               plotOutput("distPlot")
-      )
-    )
-  )
+  
+      #Tab for company risk preference
+      tabPanel("Risk Preference",
+               h1(strong("Before prediction, select your risk preference")),
+               hr(),
+               # input - company risk preference
+               titlePanel("Input - Your Risk Evaluation"),
+               p("Please evaluate the cost based on the following two kinds of risk."),
+               fluidRow(
+                 column(5,
+                        #Input risk preference
+                        sliderInput("GoodCustomer", "(i) good customer was mistakely predicted as bad customer - you lose a good customer", min=1, max=10, value = 5),
+                        sliderInput("BadCustomer", "(i) bad customer was mistakely predicted as good customer - you get a bad customer", min=1, max=10, value = 5),
+                        submitButton("Submit", icon("refresh"), width = 100 )
+                 )
+               ),
+               
+               # Output
+               br(),
+               hr(),
+               titlePanel("Output - Recommended Cut-off Point for Good/Bad Customer"), 
+               br(),
+               mainPanel(
+                 plotOutput("PredictionPlot"), 
+                 br(), br(), br()),   
+               
+               # input - selection of cutoff
+                 column(12,
+                        #Input risk preference
+                        sliderInput("i", "Please input the cutoff point to determine Good/Bad Customer. 
+                                    We suggest you to select point where the cost is the lowest.", 
+                                    min=0.01, max=0.99, value = 0.5)),
+                 br(),br()
+               ),
+        
+        
+      # Tab for prediction
+      tabPanel("Prediction",
+             h1(strong("Predict credit risk level of customers")),
+             hr(),
+             
+             # input - customer info
+             # Sidebar with a slider input for number of bins 
+             titlePanel("Input - Customer Information"),
+             fluidRow(
+                column(5,
+                 # Input attribute 1 Status of Existing checking account
+                 selectInput( "AccountBalance", "Status of existing checking account",
+                             c("...< 0 DM" = 1, "0 <= ... <= 200 DM" = 2, "... >= 200 DM" = 3, "no checking account" = 4),
+                             selected = 4), 
+                 # Input attribute 2 Status of Duration in month
+                 numericInput("DurationInMonth", "Months until credit due date", 24, min=0), 
+                 # Input attribute 3 Credit history
+                 selectInput("PaymentStatus", "Credit history", 
+                             c("no credits taken/all credits paid back duly" = 1, "all credits at this back paid back duly" = 2,
+                               "existing credits paid back duly till now" = 3, "delay in paying off in the past" = 4, "critical account/other credits existing (not at this bank)" = 5),
+                             selected = 3),
+                 # Input attribute 4 Purpose
+                 selectInput("PurposeOfCredit", "Purpose", 
+                             c("car(new)"=1, "car(used)"=2, "furniture/equipment"=3, "radio/television"=4, "domestic appliances"=5,
+                               "repairs"=6, "eucation"=7, "vacation"=8, "retraining"=9, "business"=10, "others"=11),
+                             selected=4),
+                 # Input attribute 5 Credit amount
+                 numericInput("CreditAmount", "Credit amount", 3271),
+                 # Input attribute 6 Savings account/bonds
+                 selectInput("SavingOrBonds", "Savings accountOrbonds",
+                             c("...< 100 DM"=1, "100 <= ... < 500 DM"=2, "500 <= ... < 1000 DM"=3, "...>=1000 DM"=4,"unknown/no savings account"=5),
+                             selected=1),
+                 # Input attribute 7 Present employment since
+                 selectInput("LengthOfCurrentEmployment","Present employment since",
+                             c("unemployed"=1, "... < 1 year"=2, "1 <= ... < 4 years"=3, "4 <= ... < 7 years"=4, "... >= 7 years"=5),
+                             selected=3),
+                 # Input attribute 8 Installment rate in percentage of disposable income
+                 selectInput("Installment", "Installment rate in percentage of disposable income (level from 1 to 4)", 
+                             c("1"=1, "2"=2, "3"=3, "4"=4), selected=2),
+                 # Input attribute 9 Personal status and sex
+                 selectInput("SexAndMaritalStatus", "Personal status and sex",
+                             c("male: divorced/separated"=1, "femaile: divorced/separated/married"=2, "male: single"=3,
+                               "male: married/widowed"=4, "female: single"=5),
+                             selected=3),
+                 # Input attribute 10 Other debtors/guarantors
+                 selectInput("Guarantor","Other debtors/guarantors",
+                             c("none"=1, "co-applicant"=2,"guarantor"=3),
+                             selected=1)),
+               column(5,
+                 # Input attribute 11 Present residence since
+                 numericInput("DurationInCurrentAddress","Duration in current address", 3, min=0),
+                 # Input attribute 12 Property
+                 selectInput("MostValuableAsset","Property",
+                             c("real estate"=1, "building society savings agreement/life insurance"=2, "car or other"=3, "unknown/no property"=4),
+                             selected=3),
+                 # Input attribute 13 Age in years
+                 numericInput("Age", "Age in years", 35, min=18),
+                 # Input attribute 14 Other installment plans
+                 selectInput("OtherInstallmentPlan", "Other installment plans",
+                             c("bank"=1, "stores"=2, "none"=3),
+                             selected=3),
+                 # Input attribute 15 Housing
+                 selectInput("Housing","Housing",
+                             c("rent"=1, "own"=2, "for free"=3),
+                             selected=2),
+                 # Input attribute 16 Number of existing credits at this bank
+                 numericInput("NoOfCreditsAtBank", "Number of existing credit accounts at this bank", 1, min=0),
+                 # Input attribute 17 Job
+                 selectInput("Occupation","Job",
+                             c("unemployed/unskilled--non-resident"=1, "unskilled-resident"=2, "skilled employee/official"=3,
+                               "management/self-employed/highly qualified employee/officer"=4),
+                             selected=3),
+                 # Input attribute 18 Number of perople being liable to provide maintenance for 
+                 numericInput("NoOfDependents","Number of people being liable to provide maintenance for", 1, min=0),
+                 # Input attribute 19 Telephone
+                 selectInput("Telephone","Telephone", c("none"=1,"yes,registered under the customers name"=2), selected=1),
+                 # Input attribute 20 foreign worker
+                 selectInput("ForeignWorker","Foreign worker", c("yes"=1, "no"=2), selected=1),
+                 submitButton("Submit", icon("refresh"), width = 100 )
+               )),
+               
+              # Output
+              br(),
+              hr(),
+              titlePanel("Output - Recommended Credit Risk Classification"), 
+              br(),
+              mainPanel(
+                tableOutput("PredictionTable"),
+              br(), br(), br()   
+               )
+  ),
+  
+  
+  # Tab for clustering
+  tabPanel("Clustering",
+           h1(strong("Cluster the data")), hr(),
+           titlePanel("Choose the variables:"),
+           # Sidebar with a slider input for number of bins 
+           sidebarLayout(
+             sidebarPanel(
+               checkboxInput('AccountBalance1', 'Status of existing checking account',TRUE),
+               checkboxInput('DurationInMonth1', 'Duation in month',TRUE),
+               checkboxInput('PurposeOfCredit1', 'Purpose',TRUE),
+               checkboxInput('DurationInCurrentAddress1', 'Present residence since',TRUE),
+               checkboxInput('Age1', 'Age',TRUE),
+               checkboxInput('NoOfCreditsAtBank1', 'Number of existing credits at this bank',TRUE),
+               submitButton("Submit", icon("refresh"), width = 100 )),
+             # Show the ClusterPlot
+             mainPanel(
+               plotOutput("ClusterPlot")
+             )))
 ))
